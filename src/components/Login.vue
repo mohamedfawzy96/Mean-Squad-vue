9a73be2a83e37244ab278d9329c6a58b9f24d031
ameniawy <ameniawy18@gmail.com>
2017-04-09 01:44:27 +0200

    fixed the login, a much better way

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="login-fawzy">
-        <link rel="stylesheet" href="static/css/login/login.css">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <label for="inputEmail" class="sr-only">Username</label>
-        <input type="text" v-model="username" name="username" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" v-model="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <button class="btn btn-lg btn-primary btn-block" v-on:click="login">Sign in</button>
+    <form v-on:submit="login">
+      <link rel="stylesheet" href="static/css/login/login.css">
+      <h2 class="form-signin-heading">Please sign in</h2>
+      <label for="inputEmail" class="sr-only">Username</label>
+      <input type="text" v-model="username" name="username" id="inputEmail" class="form-control" placeholder="Email address" required
+        autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" v-model="password" name="password" id="inputPassword" class="form-control" placeholder="Password"
+        required>
+      <button class="btn btn-lg btn-primary btn-block">Sign in</button>
+    </form>
   </div>
 </template>
 
 <script>
-export default {
+  export default {
     name: 'login',
-    data () {
+    data() {
       return {
         msg: 'Welcome to Your Vue.js App',
         username: '',
@@ -21,23 +25,24 @@ export default {
       }
     },
     methods: {
-      login: function() {
-          console.log(this.username);
-            this.$http.post('http://localhost:8080/user/login', {
-              username:this.username,
-              password:this.password
-            })
-            .then(function(response){
-              if(response.data.data.errors) {
-                console.log("bateekh");
-              }
+      login: function (e) {
+        console.log(this.username);
+        e.preventDefault();
+        this.$http.post('http://localhost:8080/user/login', {
+            username: this.username,
+            password: this.password
+          })
+          .then(function (response) {
+            if (response.data.data.errors) {
+              console.log("responded with errors");
+            }
             console.log(response.data.data);
-            }, function(response){
-              console.log("bayez");
-            });
+          }, function (response) {
+            console.log("error happened with http");
+          });
       }
     }
-}
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->

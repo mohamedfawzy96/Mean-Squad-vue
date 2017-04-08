eddbcd00f76d538cc8654f4e6c2ed9326a983dc4
ameniawy <ameniawy18@gmail.com>
2017-04-09 01:24:30 +0200

    example login with a very bad, but working approach. DONT USE IT

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -19,6 +19,7 @@
             <li><router-link to='/search' >Search</router-link></li>
             <li><router-link to='/businesses' >Businesses</router-link></li>
             <li><router-link to='/activities' >Activities</router-link></li>
+            <li><router-link to='/login' >Login</router-link></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
